6b77119974e892a2ab11b4bc4fd5d6369d13bd0d
Chibuikem Tochukwu <129077053+Chibuikem1204@users.noreply.github.com>
2025-05-24 02:19:53 +0100

    Create dql_change_over_time.sql

---
+++ scripts/dql_change_over_time.sql
@@ -0,0 +1,23 @@
+SELECT
+	YEAR (order_date) order_year,
+	MONTH(order_date) order_month,
+	SUM (sales_amount) Total_Sales,
+	COUNT(customer_key) Total_Customers,
+	SUM (quantity) Total_Quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR (order_date), MONTH (order_date)
+ORDER BY YEAR (order_date), MONTH (order_date)
+
+OR
+
+SELECT
+	DATETRUNC(month, order_date) order_date,
+	SUM (sales_amount) Total_Sales,
+	COUNT(customer_key) Total_Customers,
+	SUM (quantity) Total_Quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+
+ORDER BY DATETRUNC(month, order_date)
